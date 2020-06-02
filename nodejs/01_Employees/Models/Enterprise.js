abce4ea85b63c0353bd768fde02045ff7009ede8
MDevoldere <mdevoldere@devoldere.net>
2020-06-02 15:56:05 +0200

    en retard !

--- nodejs/01_Employees/Models/Enterprise.js
+++ nodejs/01_Employees/Models/Enterprise.js
@@ -10,65 +10,72 @@ class Enterprise
     }
 
     /**
-     * 
-     * @param  _filter 
+     * Récupère une liste d'employés selon un ou plusieurs filtres
+     * @param  _filter le(s) filtre(s) à appliquer
+     * @returns Employee[] La liste des employés trouvés ou une liste vide si aucune correspodance
      */
     readAll(_filter) {
-        
+        return [];
     }
 
     /**
-     * Créer un employé
+     * Créer un employé (valide les données de l'employé et l'ajoute à la collection)
      * @param Employee _employee 
+     * @returns Employee L'objet Employee ajouté
      */
     create(_employee) {
-
+        return _employee;
     }
 
     /**
-     * 
+     * Recherche un employé par son identifiant
      * @param int _id 
+     * @returns Employee l'employé correspodant ou undefined si non trouvé
      */
     read(_id) { 
-
+        return undefined;
     }
 
     /**
-     * Met à jour un employé
+     * Valide les données et met à jour un employé de la collection
      * @param Employee _employee 
      */
     update(_employee) {
-
+        return _employee;
     }
     
     /**
-     * Supprime un employé
+     * Supprime un employé identifié par "_id" de la collection
      * @param int _id 
+     * @returns bool true en cas de succès sinon, false
      */
     delete(_id) {
-
+        return true;
     }
 
 
     /**
-     * 
+     * Recherche l'employé ayant le salaire le plus élevé
+     * @returns Employee
      */
     getHigherSalary() {
-
+        return undefined;
     }
 
     /**
-     * 
+     * Recherche l'employé ayant le salaire le plus bas
+     * @returns Employee
      */
     getLowerSalary() {
-
+        return undefined;
     }
 
     /**
-     * 
+     * Retrouve la différence entre le salaire le plus elevé et le salaire le plus bas
+     * @returns int
      */
     getSalaryGap() {
-
+        return 0;
     }
 
 
