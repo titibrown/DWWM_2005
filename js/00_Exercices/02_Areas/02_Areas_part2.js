0c8434ff282fa08a9c977bcf99dc2bd1d4530b60
MDevoldere <mdevoldere@devoldere.net>
2020-06-03 07:59:41 +0200

    Areas typos

--- js/00_Exercices/02_Areas/02_Areas_part2.js
+++ js/00_Exercices/02_Areas/02_Areas_part2.js
@@ -9,37 +9,54 @@
  * 2) Implémentez le code nécessaire à son bon fonctionnement en suivant les indications dans sa descirption
  * 3) Implémentez ensuite le programme suivant :
  *      a) Créer 2 "Area"
+ *          a.1) Area 1 : 4 lignes x 4 colonnes
+ *          a.2) Area 2 : 8 lignes x 6 colonnes
  *      b) Dans la 1ère "Area", ajouter 8 "Point"" (coordonnées au choix, 5 dans les limites de la zone et 3 hors limites)
- *      c) Dans la 2nde "Area", ajouter 6 "Point"" (coordonnées au choix, 4 dans les limites de la zone et 2 hors limites)
- *      d) Créer 2 "Point" distincts ayant les mêmes coordonnées
+ *      c) Dans la 2nde "Area", ajouter 6 "Point"" (coordonnées au choix, tous en dehors des limites)
+ *      d) Créer ensuite 2 "Point" distincts ayant les mêmes coordonnées
  *      e) Les ajouter dans chacune des zones (1 "Point" par zone)
  *      f) Pour chaque zone : 
  *          - Afficher tous les points qui se trouvent hors des limites de la zone
- *          - Déplacer ensuite les points hors limite vers le point le plus proche se trouvant dans les limites de la zone :D
- *          - Afficher tous les points en les triant les 
+ *          - Exécuter la méthode "needAllInside"
+ *          - Afficher tous les points
+ *          - Afficher le nombre d'emplacements libres
  * 
  */
 
 
 /**
- * La classe "Area" représente une zone à 2 dimensions type "fenêtre".
- * La zone est définie par une largeur et une hauteur (width, height)
- * La zone peut accueillir des instances de la classe "Point" (dans un tableau ou autre)
- * On peut donc ajouter et supprimer des "Point" se trouvant dans la zone.
- * Particularités :
- * - Le "Point" d'origine de la zone (0,0) est situé au coin supérieur gauche de la zone
- * - Le "Point" d'origine existe toujours (créé dans le constructeur) et ne peut être supprimé
- * - A partir du "Point" d'origine, la zone s'étend vers la droite pour les abscisses et vers le bas pour les ordonnées
- * - 1 seul "Point" autorisé par coordonnée
- * - Les "Point" ajoutés peuvent se situer en dehors des limites de la zone
- * - Les "Point" situés sur la limite de la zone sont considérés "dans les limites de la zone" 
- * Aidez-vous des images "02_Points_***" présentes dans ce dossier pour vous faire une réprésentation
+ * Class Area
+ * Un "Point" est un objet représentant des coordonnées (x,y) dans un espace à 2 dimensions
+ * Une "Area" est un objet représentant une zone à 2 dimensions (coïncidence?)
+ * Une "Area" est obligatoirement définie par une largeur (nombre de colonnes) et une hauteur (nombre de lignes) pour former une grille.
+ * On peut ajouter, déplacer et supprimer des "Point" à l'intérieur d'une Area (1 seul "Point" par coordonnée).
+ * Une "Area" ne peut contenir plus de "Point" que de coordonnées disponibles dans la zone.
+ * (une zone 4x4 est limitée à 16 points)
+ * Les "Point" ajoutés peuvent se situer en dehors des limites de la zone (la limite du nombre de point par zone ne peut pas être dépassée !).
+ * 
+ * Le "Point" d'origine (0,0) d'une "Area" est situé au coin supérieur gauche
+ * Le "Point" d'origine existe toujours (créé dans le constructeur) et ne peut être supprimé
+ * A partir du "Point" d'origine, la zone s'étend vers la droite pour les abscisses et vers le bas pour les ordonnées
+ *
+ *
+ * ALGORITHME AJOUTER/DEPLACER UN POINT DANS UNE AREA: 
+ *
+ * 1) En cas d'ajout, si la zone (Area) est pleine (Toutes les coordonnées "dans les limites" sont occupées par un "Point")
+ *    1.1) Ajout impossible, renvoyer false
+ *
+ * 2) Si un "Point" existe déjà aux mêmes coordonnées:
+ *    2.1) Rechercher la position libre la plus proche du "Point" d'origine (0,0)
+ *    2.2) Si 2 positions son éligibles (distance égale), celle la plus proche du bord supérieur de la zone est privilégiée
+ *       
+ * 3) Enregistrer le "Point" dans la zone
+ * 
+ * 4) Renvoyer true
  */
-class Area
+class Area 
 {
     /**
      * Constructeur: Initialise une nouvelle instance de la classe "Area"
-     * La largeur et la hauteur définissent les limites de la zone. 
+     * La largeur et la hauteur définissent respectivement le nombre de colonnes et de lignes. 
      * @param int _width largeur de la zone
      * @param int _height hauteur de la zone
      */
@@ -54,7 +71,7 @@ class Area
      * @returns Boolean true en cas de succès, false si l'ajout est impossible 
      */
     addPoint(_point) {
-        if((typeof _point) !== 'Point') {
+        if ((typeof _point) !== 'Point') {
             return false;
         }
 
@@ -63,12 +80,23 @@ class Area
         return true;
     }
 
-    /**
-     * Vérifie la position de chaque "Point" enregistré
-     * Chaque Point hors des limites est automatiquement déplacé vers une position "dans la zone"
-     * Ce déplacement utilise les mêmes règles que l'ajout d'un "Point"
-     */
-    needAllInside() {
 
+    /**
+     * Déplace un point existant dans la zone vers de nouvelles coordonnées
+     * Les nouvelles coordonnées peuvent se trouver hors limites
+     * @returns Boolean true en cas de succès, false en cas d'échec
+     */
+    movePoint(/* déterminer les paramètres */) {
+        // implémenter la méthode
+    }
+
+
+    /**
+     * Vérifie la position de chaque "Point" existant dans la zone
+     * Chaque Point hors des limites est automatiquement déplacé dans les limites vers la position libre la plus proche
+     * @returns int le nombre de points déplacés
+     */
+    needAllInside(/* déterminer les paramètres */) {
+        // implémenter la méthode
     }
 }
