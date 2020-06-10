ceb1a4cfc0bf5690c634ed839f1669de21ef57aa
MDevoldere <mdevoldere@devoldere.net>
2020-06-11 00:02:32 +0200

    null is a finite Number...

--- nodejs/02_Areas/test_area.js
+++ nodejs/02_Areas/test_area.js
@@ -54,6 +54,18 @@ a2.addPoint(p2);
 
 // let triDistance = a1.points.sort(a1.closestFromOrigin);
 
+let a = 1;
+let b = NaN;
+let c = 0;
+let d;
+let e = null;
+
+console.log(isFinite(a)); // true
+console.log(isFinite(b)); // false
+console.log(isFinite(c)); // true
+console.log(isFinite(d)); // false
+console.log(isFinite(e)); // true ??????????????????????
+
 a1.needAllInside();
 
 console.log();
