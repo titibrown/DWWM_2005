4bbb11880236fee0a93a4e5ca81de27f3bfdbc99
MDevoldere <mdevoldere@devoldere.net>
2020-06-10 17:35:25 +0200

    Areas Héritage

---
+++ js/00_Exercices/02_Areas/02_Areas_part3.js
@@ -0,0 +1,40 @@
+/**
+ * JAVASCRIPT:  Exercices
+ * 
+ * Types de "Point"
+ * 
+ * Un Point représente une position.
+ * 
+ * Nous avons besoin de représenter plusieurs types de Point :
+ *  - Un mur (Wall) possédant
+ *      . un attribut supplémentaire avec accesseur et sans mutateur
+ *          material : string
+ *  - Une porte (Door) possédant
+ *      . un attribut supplémentaire avec accesseur et sans mutateur
+ *          isOpen : bool
+ *      . une méthode supplémentaire
+ *          openClose() : bool
+ *  - Un chemin (Path) possédant
+ *      . 2 attributs supplémentaires avec accesseur et mutateur
+ *          player : bool
+ *          nbPassages : int
+ *  - Une arrivée (Goal) possédant
+ *      . 1 attribut supplémentaire avec accesseur et sans mutateur
+ *          success : bool
+ * 
+ * 
+ * Travail à réaliser
+ * Créer un nouveau projet.
+ * Dans ce projet, créer un répertoire "models". Les classes suivantes sont à placer dans ce répertoire.
+ * Réécrire la classe Point en respectant le principe d'encapsulation (attributs privés + accesseurs/mutateurs)
+ * En utilisant l'héritage, implémenter les classes "Wall", "Door", "Path" et "Goal" décrite ci-dessous.
+ * Toutes ces classes héritent de Point (directement ou indirectement).
+ */
+
+
+ class Point 
+ {
+
+ }
+
+ module.exports = Point;
