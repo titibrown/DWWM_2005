d0dc852b982f27280d8dffc742ca84b3ec4ecfbf
MDevoldere <mdevoldere@users.noreply.github.com>
2020-11-16 16:14:24 +0100

    Routage Partie 1

--- php/users_simple/App/bootstrap.php
+++ php/users_simple/App/bootstrap.php
@@ -1,6 +1,26 @@
 <?php
+/**
+ * BOOTSTRAP.PHP
+ * Initialisation des composants de l'application MVC
+ */
 
+
+require ('Router.php');
 require (__DIR__.'/Models/DbConnection.php');
 require (__DIR__.'/Models/User.php');
 require (__DIR__.'/Models/Users.php');
 
+/**
+ * Fonction d'autochargement de classes
+ * @param string $_classname Le nom de la classe à charger
+ * @return bool true si la classe a été chargée correctement, false sinon.
+ */
+function autoload(string $_classname)
+{
+
+}
+
+/** 
+ * @var Router $router Initialisation de la classe Router
+ */
+$router = new Router('/users_simple/');
