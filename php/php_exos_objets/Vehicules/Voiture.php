cc9c8e325f4b0e6448102251baa4cd4914d3a434
MDevoldere <mdevoldere@users.noreply.github.com>
2020-11-23 16:32:20 +0100

    aggregation composition constructeur

--- php/php_exos_objets/Vehicules/Voiture.php
+++ php/php_exos_objets/Vehicules/Voiture.php
@@ -5,8 +5,8 @@ class Voiture
 {
     private Moteur $_moteur;
 
-    public function __contruct(string $newMarque, Moteur $newMoteur)
+    public function __contruct(Moteur $newMoteur)
     {
-        
+        $this->_moteur = $newMoteur;
     }
 }
--- php/php_exos_objets/Vehicules/test.php
+++ php/php_exos_objets/Vehicules/test.php
@@ -3,9 +3,26 @@
 require 'Moteur.php';
 require 'Voiture.php';
 
+
+/**
+ * AGGREGATION
+ * Création d'un moteur partagé 
+ * Ajout du même Moteur à 2 Voitures différentes
+ */
+
 $moteur = new Moteur();
 
 $v1 = new Voiture($moteur);
+
 $v2 = new Voiture($moteur);
 
-$v2 = new Voiture('Renault', 'megane', 1000, new Moteur('Renault', 320));
+
+
+
+
+/** COMPOSITION 
+ * L'instance du Moteur est directement créée dans le constructeur de la Voiture
+ * Chaque Voiture possède son propre Moteur
+*/
+
+$v2 = new Voiture(new Moteur());
