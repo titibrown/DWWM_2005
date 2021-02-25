712c97b3bcb570f9dc861307a36ffc76445e0cdd
MDevoldere <mdevoldere@users.noreply.github.com>
2021-02-25 11:53:11 +0100

    async testme

--- html-css/14-async/main.js
+++ html-css/14-async/main.js
@@ -1,4 +1,67 @@
+// fonction synchrone
+const salut = () => {
+    console.log('salut');
+}
+
+// fonction asynchrone
+const hello = async () => {
+    console.log('hello');
+}
+
+/**
+ * getApiData : lis le contenu json à partir de slowapi.php
+ * slowapi.php mettra 5 secondes à répondre.
+ * Le mot clé async définit la méthode "asynchrone".
+ * Lorsque cette fonction sera appelée, elle sera exécutée dans un processus parallèle non bloquant
+ * Le mot clé await met en pause l'exécution de la fonction le temps de terminer le traitement associé
+ */
+const getApiData = async () => {
+    try {
+        var response = await fetch('slowapi.php'); // await met la fonction en pause et attend la réponse de fetch() avant d'xécuter la suite de la fonction
+        var json = await response.json(); // pause et attente du résultat de response.json() 
+        console.log('Microtâche getApiData.fetch() terminé');
+        console.log(json); // affichage du résultat dans la console
+        console.log('Macrotâche getApiData() terminé');
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// exécution de getApiData()
+// la fonction est asynchrone, son exécution est placée dans la file des macrotâches.
+getApiData();
 
 
 
-console.log('Message synchrone');
+/** 
+ * @see /html-css/06-ajax/
+ * fetch est placé dans la file des microtâches
+*/
+fetch('api.json')
+.then((response) => {
+    return response.json();
+})
+.then((json) => {
+    console.log('Microtâche fetch() terminé')
+    console.log(json);
+})
+.catch((err) => {
+    console.error(err);
+});
+
+
+// setTimeout est asynchrone et placé dans la file des macrotâches 
+// dès qu'une macrotâche est terminée, sa fonction de rappel est exécutée (ci-dessous, les console.log())
+setTimeout(() => console.log('Macrotâche setTimeout 2000: HAHA'), 2000);
+setTimeout(() => console.log('Macrotâche setTimeout 0: HOHO'), 0);
+
+
+
+// instruction synchrone, sera exécutée en 1er !
+console.log('console.log synchrone: HIHI');
+
+// fonction asynchrone: placé dans la file des macrotâches
+hello();
+
+// fonction synchrone: exécutée directement
+salut();
--- html-css/14-async/slowapi.php
+++ html-css/14-async/slowapi.php
@@ -1,10 +1,10 @@
 <?php 
 
-// Décommenter la ligne suivante pour simuler une api lente.
-//sleep(10); 
+// Décommenter la ligne suivante pour simuler une réponse lente.
+sleep(5); // le script se met en pause 5 secondes.
 
 
-// {"name": "Mike", "age": 40}
+// {"name": "Mike", "age": 40}.
 exit(json_encode([
     'name' => 'Mike', 
     'age' => 40
