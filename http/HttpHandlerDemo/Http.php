373dac3de3e8ef28631ab8332336470877b96764
MDevoldere <dev@devoldere.net>
2021-03-07 11:45:28 +0100

    Prepa Semaine 10

---
+++ http/HttpHandlerDemo/Http.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace HttpHandlerDemo;
+    
+/**
+ * Gestionnaire HTTP
+ * Emet les réponses HTTP
+ */
+class Http 
+{
+    
+    /**
+     * Analyser cette méthode (que fait-elle ?)
+     */
+    static public function BCAE34FC(array $_data = []): array
+    {
+        return \array_map(function ($v) {
+            if(!\preg_match("/^[A-Za-z0-9\s]*$/", $v)) {
+                // raise error
+            }
+            return $v;
+        }, $_data);
+    }
+
+
+    /**
+     * Renvoie une réponse HTTP met fin au script
+     * @param int $_code Le code HTTP à utiliser dans la réponse
+     * @param mixed $_data Les données à inclure dans la réponse
+     */
+    static public function response(int $_code, mixed $_data): void
+    {
+
+    }
+
+}
---
+++ http/README.md
@@ -0,0 +1,9 @@
+# Protocole HTTP
+
+- Les verbes HTTP
+- Les codes de réponses 
+- Création d'un gestionnaire HTTP avec PHP
+    - Définir les entêtes 
+    - Définir un code de réponse
+    - Renvoyer une réponse explicite
+    - Utiliser la procuration (Proxy) pour définir le format de sortie
